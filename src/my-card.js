17c682ac576e9864d09bf1a94db61105dce3d0dc
sawyerw <sawyerw221@gmail.com>
2026-02-08 15:09:57 -0500

    5 styled cards
    Made style changes for 5 unique care elements

--- src/my-card.js
+++ src/my-card.js
@@ -14,24 +14,125 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.title = "Mayhem";
+    this.image = "https://upload.wikimedia.org/wikipedia/en/thumb/d/db/Lady_Gaga_-_Mayhem.png/250px-Lady_Gaga_-_Mayhem.png";
+    this.alt = "Lady Gaga";
+    this.cardButton = "Details";
+    this.btnLink = "https://hax.psu.edu";
+    this.body = "Lady Gaga's 7th studio albulm, Mayhem, explores love, chaos, fame, and identity. It was preceded by two singles: 'Disease' and 'Abracadabra'.";
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    // Read relevant attributes from the light DOM so <my-card image="..."> works reliably
+    if (this.hasAttribute('title')) this.title = this.getAttribute('title');
+    if (this.hasAttribute('image')) this.image = this.getAttribute('image');
+    if (this.hasAttribute('alt')) this.alt = this.getAttribute('alt');
+    if (this.hasAttribute('body')) this.body = this.getAttribute('body');
+    if (this.hasAttribute('btnlink')) this.btnLink = this.getAttribute('btnlink');
+    if (this.hasAttribute('cardbutton')) this.cardButton = this.getAttribute('cardbutton');
   }
 
   static get styles() {
     return css`
-      :host {
-        display: block;
-      }
+      .card {
+  border: 5px solid;
+  border-radius: 5%;
+  background-color: var(--my-card-background-color);
+  text-align: center;
+  width: 300px;
+  color: var(--my-card-text-color);
+  font-family: serif;
+}
+
+#cardList {
+  display: flex;
+}
+
+.wrapper {
+  display: flex;
+  gap: 1rem; 
+  flex-wrap: wrap; 
+}
+
+.card-image {
+  border-radius: 10%;
+  padding: 16px 16px 0px 16px;
+  margin: 4px 4px 4px 4px;
+  width: 250px;
+}
+
+.card-details {
+  text-align: left;
+  padding: 0 16px 0px 16px;
+  margin: 4px 4px 4px 4px;
+  align: left;
+}
+
+.card-title {
+  text-align: left;
+  padding: 0 16px 0px 16px;
+  margin: 4px 4px 4px 4px;
+  align: left;
+}
+
+.btn {
+  background-color: var(--my-card-btn-color);
+  text-align: center;
+  color: white;
+  font-size: 20px;
+  border-radius: 10%;
+  padding: 16px 100px 16px 100px;
+  margin: 10px 10px 16px 16px;
+}
+
+.btn:hover {
+  background-color: var(--my-card-btn-hover-color);
+}
+
+@media (min-width: 500px) and (max-width: 800px) {
+  .btn {
+    display: block;
+  }
+}
+
+@media (max-width: 500px) {
+  .card {
+    transform: scale(0.9);
+  }
+  .card img {
+    width: auto;
+    height: auto;
+  }
+   .btn {
+    display: none;
+  }
+}
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+  
+    return html`
+    <div id="wrapper">
+<div class="card">
+  <img class="card-image" src="${this.image}" alt="${this.alt}">
+  <h1 class="card-title">${this.title}</h1>
+  <div class="card-details">
+  <p>${this.body}</p>
+  </div>
+  <a href="${this.btnLink}">
+    <button class="btn">${this.cardButton}</button>
+  </a>
+  </div>
+  </div>`;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      image: { type: String },
+      alt: { type: String },
     };
   }
 }
--- src/polaris-chip.js
+++ src/polaris-chip.js
@@ -29,6 +29,7 @@ export class PolarisChip extends LitElement {
     super();
     // a variable on this object called title
     this.title = 'Chip Default';
+    this.link = 'html://example.com';
   }
 
   // CSS styles are scoped JUST to this element. This uses a technology called
@@ -51,6 +52,7 @@ export class PolarisChip extends LitElement {
 
       span {
         background-color: orange;
+        border: 1px solid gray;
         color: black;
         font-size: 24px;
         padding: 16px;
@@ -83,7 +85,12 @@ export class PolarisChip extends LitElement {
     // it is going to print the title of the element. The magic of Lit is that
     // when title is changed (even by inspecting the document and hacking the value)
     // it will automatically update what is displayed and do so incredibly quickly
-    return html`<span>${this.title}</span>`;
+    return html`
+    <span>
+      <a href="${this.link}">
+        ${this.title}
+      </a>
+      </span>`;
   }
 
   // LitElement uses the properties call to do the following:
@@ -95,6 +102,7 @@ export class PolarisChip extends LitElement {
     return {
       // this is a String. Array, Object, Number, Boolean are other valid values here
       title: { type: String },
+      link: { type: String },
     };
   }
 }
